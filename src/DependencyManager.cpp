e197dbadd057ad7aab444cc8ae1e3712e3a47f8d
Cameron Chitsaz <camcorder09@gmail.com>
2018-11-12 19:49:13 -0800

    added dep mgr implementation

---
+++ src/DependencyManager.cpp
@@ -0,0 +1,29 @@
+#include "DependencyManager.h"
+
+namespace Cast {
+
+DependencyManager::DependencyManager() {
+}
+
+DependencyManager::~DependencyManager() {
+}
+
+void DependencyManager::clear() {
+  deps_.clear();
+}
+
+void DependencyManager::addLib(const std::string &libName,
+                               const std::vector<std::string> &headers) {
+  deps[libName].insert(headers.begin(), headers.end()); 
+}
+
+void DependencyManager::determineDepLibs(const std::string &sourceFile,
+                                         std::set<std::string> &libs) const {
+
+  //
+  // TODO - need to determine algorithm(!)
+  //
+
+}
+
+}
