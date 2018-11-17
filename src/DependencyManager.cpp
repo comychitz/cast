998649c7864c048f00e5d30a2c015e18b4e0360d
Cameron Chitsaz <camcorder09@gmail.com>
2018-11-17 09:50:09 -0800

    more skeleton code for dependency resolving

--- src/DependencyManager.cpp
+++ src/DependencyManager.cpp
@@ -1,4 +1,5 @@
 #include "DependencyManager.h"
+#include "Util.h"
 
 namespace Cast {
 
@@ -18,16 +19,33 @@ void DependencyManager::addLib(const std::string &libName,
   headers_.insert(headers.begin(), headers.end());
 }
 
+static void parseIncludedHeaders(const std::string &source,
+                                 std::set<std::string> &headers) {
+  //
+  // TODO
+  //
+}
+
 void DependencyManager::determineDepLibs(const std::string &sourceFile,
                                          std::set<std::string> &libs) const {
 
   // given a source file, we need to find the headers it depends on. 
   // for each header, if the header file is in our dependency map, then
   // recursively call this function on that header.
+  std::set<std::string> headers;
+  parseIncludedHeaders(sourceFile, headers);
 
-  //
-  // TODO
-  //
+  for (auto &header : headers) {
+    if (headers_.find(header) != headers_.end()) {
+
+      // TODO
+      // need to add libraries this header depends on in set to return
+
+      // need to find location of header file
+
+      determineDepLibs(header, libs);
+    }
+  }
 }
 
 }
